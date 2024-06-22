185f8a3afc580a2de54f0b63c6d8359791b4efd1
Teghpreet Singh Mago <teg3001@gmail.com>
2024-06-22 19:44:40 -0400

    finished testing

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -17,27 +17,74 @@ afterEach(() => {
 });
 
 
-
-
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "Duplicate Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  fireEvent.change(inputTask, { target: { value: "Duplicate Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getAllByText(/Duplicate Test/i);
+  expect(check.length).toBe(1);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: ""}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.queryByText(/History Test/i);
+  const checkDate = screen.queryByText(new RegExp(dueDate, "i"));
+  expect(check).toBeNull(); 
+  expect(checkDate).toBeNull(); 
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "";
+  fireEvent.change(inputTask, { target: { value: "Geography Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.queryByText(/Geography Test/i); // This should be queryByText since the element should not exist
+  expect(check).toBeNull(); // The element should not exist
  });
-
-
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "Delete Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+  const check = screen.queryByText(/Delete Test/i);
+  expect(check).toBeNull();
  });
-
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2020";
+  fireEvent.change(inputTask, { target: { value: "Color Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const card = screen.getByTestId(/Color Test/i);
+  expect(card.style.backgroundColor).toBe('rgb(252, 207, 207)'); //RGB for #fccfcf
  });
--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -55,6 +55,7 @@ class AddTodo extends Component {
       // 4. The value of the text field also should reflect the local state of this component.
       <div>
         <TextField
+          data-testid="new-item-input"
           label="Add New Item"
           variant="outlined"
           onChange={this.handleChange}
@@ -70,12 +71,14 @@ class AddTodo extends Component {
             />
         </LocalizationProvider>
         <Button
+          data-testid="new-item-button"
+          role="button"
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
           variant="contained"
           color="primary"
         >
-          Add
+          Add Button
         </Button>
       </div>
     );
